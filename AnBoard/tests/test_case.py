25a51c45992c559e0b4452041dc1c8fbcb77c039
Anarion-zuo <anarionzuo@outlook.com>
2022-08-04 14:31:08 +0800

    windows linux compat

--- AnBoard/tests/test_case.py
+++ AnBoard/tests/test_case.py
@@ -1,9 +1,9 @@
-from ast import Assert
 from importlib import import_module
 from sre_constants import ASSERT
 from reg_state import RegisterState
 import make_instr_mem_hex
 import os
+import platform
 
 
 class TestCase:
@@ -15,12 +15,19 @@ class TestCase:
     def run_test(self) -> bool:
         make_instr_mem_hex.make_mem_hex(self.name)
         print("==simulation begins==")
-        simulate_path = "AnBoard.sim\\sim_1\\behav\\xsim\\"
+        simulate_path = "AnBoard.sim/sim_1/behav/xsim/"
         cur_path = os.getcwd()
         os.chdir(simulate_path)
-        os.system("call xvlog  --incr --relax -prj RISCV_vlog.prj -log xvlog.log")
-        os.system("call xelab  --incr --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot RISCV_behav xil_defaultlib.RISCV xil_defaultlib.glbl -log elaborate.log")
-        os.system("call xsim  RISCV_behav -key {Behavioral:sim_1:Functional:RISCV} -tclbatch RISCV.tcl -log simulate.log")
+
+        run_prefix = ""
+        if platform.system() == "Windows":
+            run_prefix = "call"
+        else:
+            run_prefix = ""
+
+        os.system(run_prefix + " xvlog  --incr --relax -prj RISCV_vlog.prj -log xvlog.log")
+        os.system(run_prefix + " xelab  --incr --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot RISCV_behav xil_defaultlib.RISCV xil_defaultlib.glbl -log elaborate.log")
+        os.system(run_prefix + " xsim  RISCV_behav -key {Behavioral:sim_1:Functional:RISCV} -tclbatch RISCV.tcl -log simulate.log")
         os.chdir(cur_path)
         print("==simulation ends==")
         self.reg_state.update_by_dump()
